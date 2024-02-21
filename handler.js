c8c5a961f2ca75cee72841f1bee46cf2d332d888
nidhi433 <69976535+nidhi433@users.noreply.github.com>
2024-02-21 16:57:03 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ const add = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Hello, Serverlesss1!'
+      message: 'Hello, Serverlesss!'
     }),
   };
 };
